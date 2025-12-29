7e4e0f38e818ff961800b45ab2755b469da12d65
MohsinCodasol <mohsin@codasol.com>
2025-12-29 19:02:50 +0530

    Update excelApi.js

--- src/api/excelApi.js
+++ src/api/excelApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const LocalURL = "http://localhost:8000/api/excel";
-//const LocalURL = "https://2w2kvbg9-8000.inc1.devtunnels.ms/api/excel";
+//const LocalURL = "http://localhost:8000/api/excel";
+const LocalURL = "https://2w2kvbg9-8000.inc1.devtunnels.ms/api/excel";
 
 const API = axios.create({
   baseURL: LocalURL,
